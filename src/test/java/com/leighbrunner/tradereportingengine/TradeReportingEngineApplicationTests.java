866ad4201a636dddb3c2ce61a554ec8febf45196
Leigh Brunner <leighbrunner@gmail.com>
2024-04-11 20:58:34 +1000

    Initial Commit

---
+++ src/test/java/com/leighbrunner/tradereportingengine/TradeReportingEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leighbrunner.tradereportingengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeReportingEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
